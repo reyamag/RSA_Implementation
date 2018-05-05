43ee206c023917b236440d012cac2e11fb75dabd
Reyniel Maglian <renrenmaglian@gmail.com>
2018-05-05 15:15:44 -0700

    Updated libraries to use

--- signer.py
+++ signer.py
@@ -1,6 +1,7 @@
 import os, random, struct
 import sys
 from Crypto.Cipher import AES
+from Crypto.Cipher import DES
 from Crypto.PublicKey import RSA 
 from Crypto.Signature import PKCS1_v1_5 
 from Crypto.Hash import SHA512
